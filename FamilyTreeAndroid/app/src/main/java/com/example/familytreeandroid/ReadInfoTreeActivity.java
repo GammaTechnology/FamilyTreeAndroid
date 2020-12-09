c6bb496a9b22a7cc7a2d32d30ce25d5140ecf96e
Brian Heiskaanen <maksimfomov26@gmail.com>
2020-12-09 10:01:13 +0300

    Realized cliock from button

---
+++ FamilyTreeAndroid/app/src/main/java/com/example/familytreeandroid/ReadInfoTreeActivity.java
@@ -0,0 +1,14 @@
+package com.example.familytreeandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadInfoTreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_info_tree);
+    }
+}
--- FamilyTreeAndroid/app/src/main/java/com/example/familytreeandroid/ViewTreeFragment.java
+++ FamilyTreeAndroid/app/src/main/java/com/example/familytreeandroid/ViewTreeFragment.java
@@ -1,14 +1,20 @@
 package com.example.familytreeandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ViewTreeFragment extends Fragment {
+    
+    private Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,18 @@ public class ViewTreeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_tree, container, false);
+
+
+        View v = inflater.inflate(R.layout.fragment_view_tree, container, false);
+
+        button = v.findViewById(R.id.btnI);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ViewInfoTreeActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
